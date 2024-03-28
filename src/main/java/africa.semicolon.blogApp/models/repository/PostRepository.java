45d628ce6c92e54aef2949bd373cde7d41ceaafd
love-ara <aramideotenaike@gmail.com>
2024-03-28 11:31:21 +0100

    commit

--- src/main/java/africa.semicolon.blogApp/models/repository/PostRepository.java
+++ src/main/java/africa.semicolon.blogApp/models/repository/PostRepository.java
@@ -2,6 +2,8 @@ package africa.semicolon.blogApp.models.repository;
 
 import africa.semicolon.blogApp.models.data.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 }
--- src/main/java/africa.semicolon.blogApp/models/repository/UserRepository.java
+++ src/main/java/africa.semicolon.blogApp/models/repository/UserRepository.java
@@ -2,7 +2,9 @@ package africa.semicolon.blogApp.models.repository;
 
 import africa.semicolon.blogApp.models.data.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByUsername(String username);
 }
