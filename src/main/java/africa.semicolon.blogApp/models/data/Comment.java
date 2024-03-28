f56b4c453d06ea196385d1d945f0909e38251e4e
love-ara <aramideotenaike@gmail.com>
2024-03-28 07:26:57 +0100

    commit

---
+++ src/main/java/africa.semicolon.blogApp/models/data/Comment.java
@@ -0,0 +1,15 @@
+package africa.semicolon.blogApp.models.data;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("Comments")
+public class Comment {
+    @Id
+    private String id;
+    private User commenter;
+    private String comment;
+
+}
