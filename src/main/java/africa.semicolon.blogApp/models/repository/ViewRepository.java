312cb9af591ba652ea31a22da906d90b9025c4d1
love-ara <aramideotenaike@gmail.com>
2024-03-28 11:31:39 +0100

    commit

--- src/main/java/africa.semicolon.blogApp/models/repository/ViewRepository.java
+++ src/main/java/africa.semicolon.blogApp/models/repository/ViewRepository.java
@@ -2,6 +2,8 @@ package africa.semicolon.blogApp.models.repository;
 
 import africa.semicolon.blogApp.models.data.View;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ViewRepository extends MongoRepository<View, String> {
 }
