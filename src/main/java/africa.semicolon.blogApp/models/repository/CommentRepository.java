af9adeb5fa4ea5dd03de8df1a6498bb0c0510791
love-ara <aramideotenaike@gmail.com>
2024-03-28 11:31:06 +0100

    commit

--- src/main/java/africa.semicolon.blogApp/models/repository/CommentRepository.java
+++ src/main/java/africa.semicolon.blogApp/models/repository/CommentRepository.java
@@ -2,7 +2,9 @@ package africa.semicolon.blogApp.models.repository;
 
 import africa.semicolon.blogApp.models.data.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
 }
